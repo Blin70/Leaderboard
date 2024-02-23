721defda762b6c2571fe00c11638514b6d38ebfb
Blin Axhirexha <blini.axhirexha@gmail.com>
2024-02-23 22:20:27 +0100

    Adding table scrollbar when there are a lot of rows

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Table from './Components/Table';
 
 function App(){
     return(
-        <div className="flex justify-center items-center h-screen">
-            <Table/>
+        <div className="flex justify-center items-center w-full h-screen">
+            <div style={{maxHeight:"802px"}} className='block h-full w-full justify-center items-center overflow-auto'>
+                <Table/>
+            </div>
         </div>
     );
 };
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -4,18 +4,27 @@ function Table(){
         { Rank: '2', Player: 'Test2', RR: '130', Wins: '13', GamesPlayed: '20'},
         { Rank: '3', Player: 'Test3', RR: '130', Wins: '13', GamesPlayed: '20'},
         { Rank: '4', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '5', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '6', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '7', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '8', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '9', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '10', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '11', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '12', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '13', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
     ];
 
     const HeadData = ['Rank', 'Player', 'RR', 'Wins', 'Games Played'];
 
     
     const renderedHeadData = HeadData.map((data, index) => {
-        return <th key={index}>{data}</th>
+        return <th key={index} className="border border-solid border-black border-collapse">{data}</th>
     });
 
     const renderedBodyData = BodyData.map((data, index) => {
         const renderedRowBodyData = Object.values(data).map((value, n) => {
-            return <td key={n}>{value}</td>
+            return <td key={n} className="border border-solid border-black border-collapse">{value}</td>
         });
 
         return (
@@ -26,8 +35,8 @@ function Table(){
     });
 
     return(
-            <table className="w-1/2 h-1/2 border-2 border-solid border-black text-center text-2xl overflow-visible border-collapse">
-                <thead className="border-4 border-solid border-black position-sticky">
+            <table className="w-1/2 border-2 border-solid border-black text-center text-2xl border-collapse">
+                <thead className="border-2 border-solid border-black sticky top-0">
                     <tr>
                         {renderedHeadData}
                     </tr>
