c59adc45cf7fbfc03b0e22d8d592152f58534fe4
Blin Axhirexha <blini.axhirexha@gmail.com>
2024-02-27 18:53:06 +0100

    making the table reusable

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import './app.css';
 import Table from './Components/Table';
 
 function App(){
+    const BodyData = [
+        { Rank: '1', Player: 'Test1', RR: '130', Wins: '13', GamesPlayed: '20'},
+        { Rank: '2', Player: 'Test2', RR: '130', Wins: '13', GamesPlayed: '20'},
+        { Rank: '3', Player: 'Test3', RR: '130', Wins: '13', GamesPlayed: '20'},
+        { Rank: '4', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '5', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '6', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '7', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '8', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '9', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '10', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '11', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '12', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
+        { Rank: '13', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'}
+    ];
+
+    const HeadData = ['Rank', 'Player', 'RR', 'Wins', 'Games Played'];
+
+    
+    
     return(
             <div style={{maxHeight:"802px"}} className='flex h-full w-full justify-center items-center overflow-auto'>
-                <Table/>
+                <Table BodyData={BodyData} HeadData={HeadData}/>
             </div>
     );
 };
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,23 +1,4 @@
-function Table(){
-    const BodyData = [
-        { Rank: '1', Player: 'Test1', RR: '130', Wins: '13', GamesPlayed: '20'},
-        { Rank: '2', Player: 'Test2', RR: '130', Wins: '13', GamesPlayed: '20'},
-        { Rank: '3', Player: 'Test3', RR: '130', Wins: '13', GamesPlayed: '20'},
-        { Rank: '4', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '5', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '6', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '7', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '8', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '9', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '10', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '11', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '12', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'},
-        { Rank: '13', Player: 'Test4', RR: '135', Wins: '13', GamesPlayed: '20'}
-    ];
-
-    const HeadData = ['Rank', 'Player', 'RR', 'Wins', 'Games Played'];
-
-    
+function Table({BodyData, HeadData}){
     const renderedHeadData = HeadData.map((data, index) => {
         return <th key={index} className="border border-solid border-black border-collapse">{data}</th>
     });
@@ -35,7 +16,7 @@ function Table(){
     });
 
     return(
-            <table className="table w-1/2 border-2 border-solid border-black text-center text-2xl border-collapse m-auto">
+            <table className="w-1/2 border-2 border-solid border-black text-center text-2xl border-collapse m-auto">
                 <thead className="border-2 border-solid border-black sticky top-0">
                     <tr>
                         {renderedHeadData}
